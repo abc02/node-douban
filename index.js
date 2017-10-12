51c875081a1e02676d731b555f7e24e6850f6578
abc02 <abcqq02@qq.com>
2017-10-12 16:53:47 +0800

    edit services/crawler.js fetchSingleDoubanTopic fn & other.js

--- index.js
+++ index.js
@@ -3,12 +3,16 @@ const Topic = require('./models/topic.js')
 const crawler = require('./services/crawler.js')
 
 const start = async () => {
-    let results = await crawler.fetchSingleDoubanList(0)
-    
-    for(let j = 0; j < results.length; j++){
-        console.log(j +    results[j].url)
-        await Topic.create(results[j])
+
+    for(let i =0; i < 2; i++){
+        let results = await crawler.fetchSingleDoubanList(i)
+        console.log(results)
+        // for(let j =0; j < results.length; j++){
+        //     await crawler.fetchSingleDoubanTopic(results[j].url)
+
+        // }
     }
+  
 }
 
 
